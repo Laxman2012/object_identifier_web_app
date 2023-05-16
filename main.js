6ca0d91191a472cdbdee5187f8c66bea6b6bea90
Laxman2012 <110541419+Laxman2012@users.noreply.github.com>
2023-05-16 21:00:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function Btn1()
+{
+
+    window.location.href = "book.html";
+
+}
+
+function Btn2()
+{
+
+    window.location.href = "cable.html";
+
+}
+
+function Btn3()
+{
+
+    window.location.href = "pen.html";
+
+}
+
+function Btn4()
+{
+
+    window.location.href = "phone.html";
+
+}
+
+function Btn5()
+{
+
+    window.location.href = "shampoo.html";
+
+}
---
+++ phone.js
@@ -0,0 +1,56 @@
+img ="";
+Status = "";
+
+function setup()
+{
+
+   canvas = createCanvas(640 , 420);
+   canvas.center();
+
+   objectidentifier = ml5.objectDetector('cocossd' , modelLoaded );
+   document.getElementById("status").innerHTML = "Status : Detecting object";
+
+}
+
+function preload()
+{
+
+    img = loadImage("phone.jpg");
+
+}
+
+function modelLoaded()  
+{
+    
+    console.log("Model Loaded!!!");
+    Status = true;
+    objectidentifier.detect(img , gotresult);
+
+}
+
+function back()
+{
+
+    window.location.href = "index.html";
+
+}
+
+function draw()
+{
+
+   image(img,0,0,640,420);
+}
+
+function gotresult(error , results)
+{
+
+ if(error)
+ {
+
+   console.log(error);
+
+ }
+ 
+ console.log(results);
+
+}
